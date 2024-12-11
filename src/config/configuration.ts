0facda83496591814cc2cc0b6536347ee86f6fca
Njenga Muriu <smuriu@tuta.io>
2024-12-11 17:56:10 +0300

    explicitly specify PORT as env variable in zerops deploy

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,5 +1,5 @@
 export default () => ({
-  port: parseInt(process.env.PORT) || 3000,
+  port: parseInt(process.env.PORT ?? '3000', 10),
   supabase: {
     url: process.env.SUPABASE_URL,
     key: process.env.SUPABASE_KEY,
