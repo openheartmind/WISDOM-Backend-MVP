26f4d5f7ecb8e2cb2ff6a0e139cd5dd4c4374aff
Mohammed Muntaser <mmuntaser@gmail.com>
2025-04-29 14:54:59 +0300

    fix: added validation pipeline, validations for dto

--- src/instance/dto/add-member.dto.ts
+++ src/instance/dto/add-member.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { roles } from '../instance.roles';
+import { IsEnum } from 'class-validator';
 
 export class AddMemberDto {
+  @IsEnum(roles)
   @ApiProperty({
-    example: '0',
+    example: 'Manager',
     description: 'The id of the role to be assigned to the user',
+    enum: roles,
   })
   role: string;
   @ApiPropertyOptional()
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
 
   const logger = new Logger('Main');
