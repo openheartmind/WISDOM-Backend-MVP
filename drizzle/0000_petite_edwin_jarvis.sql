53c33fcccc3fb51096aa6f8c293e67a40ab5b792
Samson Barratt <samsonbarratt@gmail.com>
2025-05-27 18:29:33 +1000

    Fixed migration history after rebase

--- drizzle/0000_petite_edwin_jarvis.sql
+++ drizzle/0000_petite_edwin_jarvis.sql
@@ -35,6 +35,18 @@ CREATE TABLE "memberships" (
 	"updated_at" timestamp DEFAULT now()
 );
 --> statement-breakpoint
+CREATE TABLE "contributions" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"title" text NOT NULL,
+	"content" text NOT NULL,
+	"created_at" timestamp DEFAULT now(),
+	"updated_at" timestamp DEFAULT now(),
+	"contributor_id" text,
+	"instance_id" uuid NOT NULL
+);
+--> statement-breakpoint
 ALTER TABLE "instances" ADD CONSTRAINT "instances_created_by_users_id_fk" FOREIGN KEY ("created_by") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE "memberships" ADD CONSTRAINT "memberships_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "memberships" ADD CONSTRAINT "memberships_instance_id_instances_id_fk" FOREIGN KEY ("instance_id") REFERENCES "public"."instances"("id") ON DELETE no action ON UPDATE no action;
+ALTER TABLE "memberships" ADD CONSTRAINT "memberships_instance_id_instances_id_fk" FOREIGN KEY ("instance_id") REFERENCES "public"."instances"("id") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "contributions" ADD CONSTRAINT "contributions_contributor_id_users_authId_fk" FOREIGN KEY ("contributor_id") REFERENCES "public"."users"("authId") ON DELETE no action ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "contributions" ADD CONSTRAINT "contributions_instance_id_instances_id_fk" FOREIGN KEY ("instance_id") REFERENCES "public"."instances"("id") ON DELETE cascade ON UPDATE no action;
