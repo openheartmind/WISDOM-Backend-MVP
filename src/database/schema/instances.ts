d6d40b79f4e848d1844c4822d1d98248529dc23b
dipanshuhappy <dipanshuhappy@gmail.com>
2025-07-29 16:33:47 +0530

    Add TODO comment for instance status enum

--- src/database/schema/instances.ts
+++ src/database/schema/instances.ts
@@ -7,6 +7,7 @@ export const pgInstances = pgTable('instances', {
   title: text('title').notNull(),
   description: text('description').notNull(),
   createdBy: uuid('created_by').references(() => users.id),
+  //TODO: Add status enum,ideally enum(prereview, in_review, completed)
   status: text('status'),
   createdAt: timestamp('created_at').defaultNow(),
   updatedAt: timestamp('updated_at').defaultNow(),
